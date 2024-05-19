4e7994d44023d86a403b374ad6971bffa8fc690d
leuuis <louis.anime@gmail.com>
2024-05-18 18:11:35 -0600

    Initial Commit

---
+++ GameStream/GameStreamApp.swift
@@ -0,0 +1,17 @@
+//
+//  GameStreamApp.swift
+//  GameStream
+//
+//  Created by elouis on 5/18/24.
+//
+
+import SwiftUI
+
+@main
+struct GameStreamApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
