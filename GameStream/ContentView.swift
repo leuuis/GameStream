327a34fb79215eb5e6a8f8fae825096de010e371
leuuis <louis.anime@gmail.com>
2024-05-19 13:09:49 -0600

    SignUp view added with their  form

--- GameStream/ContentView.swift
+++ GameStream/ContentView.swift
@@ -35,7 +35,7 @@ struct ContentView: View {
 
 struct SignInSignUpView: View{
     
-    @State var inSignInView:Bool = true
+    @State var inSignInView:Bool = false
     
     var body: some View {
         
@@ -88,12 +88,152 @@ struct SignInSignUpView: View{
 
 struct SignUpView: View {
     
+    @State var email:String = ""
+    @State var _password:String = ""
+    @State var _passwordConfirm:String = ""
+    
     var body: some View {
         
-        Text("Pantalla registro")
+        ScrollView(){
+            
+            VStack(){
+                
+                Text("Elije una foto de perfil")
+                    .fontWeight(.bold)
+                    .foregroundColor(.white)
+                
+                Text("Puedes cambiar o elegirla más adelante")
+                    .font(.footnote)
+                    .fontWeight(.light)
+                    .foregroundColor(.gray).padding(.bottom)
+                
+                
+                Button(action: takePicture, label: {
+                    
+                    ZStack {
+                        
+                        Image("FotoPrueba").resizable()
+                            .aspectRatio(contentMode: .fit)
+                            .frame(width: 80, height: 80)
+                        
+                        Image(systemName: "camera").foregroundColor(.white)
+                    }
+                    
+                }).padding(.bottom)
+            }
+            
+            VStack (alignment: .leading){
+                
+                Text("Correo Electrónico*").foregroundColor(Color("DarkCyan"))
+                
+                ZStack(alignment: .leading){
+                    
+                    if email.isEmpty{
+                        
+                        Text("ejemplo@gmail.com")
+                            .font(.caption)
+                            .foregroundColor(.gray)
+                        
+                    }
+                    
+                    TextField("", text: $email).keyboardType(.emailAddress)
+                    
+                }
+                
+                Divider().frame(height: 1).background(Color("DarkCyan")).padding(.bottom)
+                
+                Text("Contraseña*").foregroundColor(Color("DarkCyan"))
+                
+                ZStack(alignment: .leading){
+                    
+                    if _password.isEmpty{
+                        
+                        Text("Escribe tu contraseña")
+                            .font(.caption)
+                            .foregroundColor(.gray)
+                        
+                    }
+                    
+                    SecureField("", text: $_password)
+                    
+                }
+                
+                Divider().frame(height: 1).background(Color("DarkCyan")).padding(.bottom)
+                
+                Text("Confirmar contraseña*").foregroundColor(Color("DarkCyan"))
+                
+                ZStack(alignment: .leading){
+                    
+                    if _password.isEmpty{
+                        
+                        Text("Confirma tu contraseña")
+                            .font(.caption)
+                            .foregroundColor(.gray)
+                        
+                    }
+                    
+                    SecureField("", text: $_passwordConfirm)
+                    
+                }
+                
+                Divider().frame(height: 1).background(Color("DarkCyan")).padding(.bottom)
+                
+                Button(action: signUp, label: {
+                    Text("REGÍSTRATE")
+                        .fontWeight(/*@START_MENU_TOKEN@*/.bold/*@END_MENU_TOKEN@*/)
+                        .foregroundColor(.white)
+                        .frame(maxWidth: .infinity, alignment: .center)
+                        .padding(EdgeInsets(
+                            top: 11, leading: 18, bottom: 11, trailing: 18))
+                        .overlay(RoundedRectangle(cornerRadius: 6.0)
+                            .stroke(Color("DarkCyan"), lineWidth: 1.0).shadow(color:.white, radius: 6))
+                }).padding(.bottom)
+                
+                Text("Registrate con redes sociales").frame( maxWidth: .infinity,  alignment: .center).foregroundColor(.white)
+                
+                HStack(){
+                    
+                    Button(action: {print ("Inicio de sesión con Facebook") }, label: {
+                        Text("Facebook")
+                            .font(.subheadline)
+                            .fontWeight(/*@START_MENU_TOKEN@*/.bold/*@END_MENU_TOKEN@*/)
+                            .foregroundColor(.white)
+                            .padding(.vertical, 3.0)
+                            .frame(maxWidth: .infinity, alignment: .center)
+                            .background(Color("BlueGray"))
+                            .clipShape(RoundedRectangle(cornerRadius: 4.0))
+                            .padding()
+                    })
+                    
+                    Button(action: {print ("Inicio de sesión con Twitter") }, label: {
+                        Text("Twitter")
+                            .font(.subheadline)
+                            .fontWeight(/*@START_MENU_TOKEN@*/.bold/*@END_MENU_TOKEN@*/)
+                            .foregroundColor(.white)
+                            .padding(.vertical, 3.0)
+                            .frame(maxWidth: .infinity, alignment: .center)
+                            .background(Color("BlueGray"))
+                            .clipShape(RoundedRectangle(cornerRadius: 4.0))
+                            .padding()
+                    })
+
+                    
+                }
+                
+            }.padding(.horizontal, 77.0)
+        }
+
         
     }
     
+}
+
+func takePicture() {
+    print("tomando foto...")
+}
+
+func signUp() {
+    print("registrandome...")
 }
 
 struct SignInView: View {
